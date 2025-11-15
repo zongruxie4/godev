725b4671f371ca23f446cd53c77eca88adf91f50
Juan Beleño <juanb7737@gmail.com>
2025-11-15 19:00:41 -0300

    refactor install script test for improved Windows compatibility and clean up go.mod replacements

--- install_test.go
+++ install_test.go
@@ -1,33 +1,21 @@
 package golite
 
 import (
-	"os"
 	"os/exec"
 	"runtime"
 	"testing"
 )
 
 func TestInstallScript(t *testing.T) {
-
 	var cmd *exec.Cmd
 
-	switch runtime.GOOS {
-	case "windows":
-		// En Windows, intentamos primero con Git Bash
-		gitBashPath := "C:\\Program Files\\Git\\bin\\bash.exe"
-		if _, err := os.Stat(gitBashPath); err == nil {
-			cmd = exec.Command(gitBashPath, "-c", "./install.sh")
-		} else {
-			// Si no encuentra Git Bash, intenta con WSL
-			cmd = exec.Command("wsl", "./install.sh")
-		}
-	default:
-		// Para Linux y macOS
+	if runtime.GOOS == "windows" {
+		// En Windows, usa la ruta completa de Git Bash
+		cmd = exec.Command("C:\\Users\\juan7\\AppData\\Local\\Programs\\Git\\bin\\bash.exe", "./install.sh")
+	} else {
+		// En Linux/Mac usa bash normal
 		cmd = exec.Command("bash", "./install.sh")
 	}
-
-	// Configurar el directorio de trabajo
-	cmd.Dir = "." // Asegúrate de que esto apunta al directorio correcto
 
 	// Capturar tanto stdout como stderr
 	output, err := cmd.CombinedOutput()
